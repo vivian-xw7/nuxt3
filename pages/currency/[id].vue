7a62402d6750f63f13902184b965ed1d112d2511
Vivian Hayes <vivian.xw7@gmail.com>
2024-04-09 19:41:12 -0500

    boiler plates

---
+++ pages/currency/[id].vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <!-- Your template code here -->
+      <h1>{{ pageTitle }}</h1>
+      <p>{{ pageContent }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        pageTitle: 'Currency Page',
+        pageContent: 'This is the content of the currency page.'
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  /* Your scoped styles here */
+  </style>
+  
